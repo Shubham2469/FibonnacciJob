9c50a39a0268469bddda498636135ec7495c83bb
Shubham2469 <shubhpawaskarsp24@gmail.com>
2021-06-25 15:49:19 +0530

    added new project files of NUnitDemo project

---
+++ NUnitDemo/NUnitDemo/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnitDemoConsole;
+using NUnit.Framework;
+
+
+namespace NUnitDemo
+{
+    [TestFixture]
+    public class Class1
+    {
+
+        Fibonacci fObj = null;
+
+        public Class1()
+        {
+
+            fObj = new Fibonacci();
+
+        }
+
+        [Test]
+        public void FibonacciPass()
+        {
+            List<int> ExpectedResult = new List<int>();
+            var result = fObj.GetFibonacci(1);
+            ExpectedResult.Add(0);
+            ExpectedResult.Add(1);
+            ExpectedResult.Add(1);
+            CollectionAssert.AreEqual(ExpectedResult, result);
+        }
+        [Test]
+        public void FibonacciFail()
+        {
+            List<int> ExpectedResult = new List<int>();
+            var result = fObj.GetFibonacci(1);
+            ExpectedResult.Add(0);
+            ExpectedResult.Add(1);
+            ExpectedResult.Add(1);
+            ExpectedResult.Add(2);
+            CollectionAssert.AreEqual(ExpectedResult, result);
+        }
+    }
+}
+    
+
---
+++ NUnitDemo/NUnitDemoConsole/Fibonacci.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitDemoConsole
+{
+    public class Fibonacci
+    {
+            public List<int> GetFibonacci(int number)
+            {
+                List<int> res = new List<int>();
+                int firstNumber = 0;
+                int secondNumber = 1;
+                int result = 0;
+                Console.WriteLine(firstNumber);
+                Console.WriteLine(secondNumber);
+                res.Add(firstNumber);
+                res.Add(secondNumber);
+                for (int i = 0; i < number; i++)
+                {
+                    result = firstNumber + secondNumber;
+                    firstNumber = secondNumber;
+                    secondNumber = result;
+                    res.Add(result);
+
+                }
+                return res;
+
+            }
+
+        }
+}
